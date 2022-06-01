38922b3fdb8fb8318a0afdb38037e832442362ea
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-06-01 17:27:29 -0500

    setup html and css using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import './App.css';
-import Button  from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Button onClick={() => {
-      console.log("clicked")
-    }}>Test</Button>
+    <div className="app">
+      <h2 className="heading">Software Developer Staff</h2>
+      <p>
+        Add to the table with a name and a job title. Remove by clicking the
+        delete button.
+      </p>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Job</th>
+            <th scope="col">Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Mohamed</td>
+            <td>Instructor</td>
+            <td>
+              <Button>Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>Mohamed</td>
+            <td>Instructor</td>
+            <td>
+              <Button>Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>Mohamed</td>
+            <td>Instructor</td>
+            <td>
+              <Button>Delete</Button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h2 className="heading">Add New</h2>
+      <h5 className="h5-heading">Name</h5>
+      <input type="text" class="form-control" />
+      <h5 className="h5-heading">Job</h5>
+      <input type="text" class="form-control" />
+    </div>
   );
 }
 
